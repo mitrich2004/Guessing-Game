20c9302df4612f49d821ca9e6bfaa2a0d09e721c
Sergey Miachkov <miachkov2004@gmail.com>
2023-05-29 21:46:56 +0200

    Sign Game and some safety nets
    Added the second game (sign game) and added more checks for the user with some other minor improvements

--- main.cpp
+++ main.cpp
@@ -1,5 +1,24 @@
 #include <iostream>
 #include <random>
+#include <vector>
+#include <algorithm>
+
+std::string won_text = "Congratulations! You won $";
+std::string lost_text = "Sorry, you lost your $";
+std::string bad_input_text = "Stop joking around, this is a serious place.\n";
+
+bool check_if_number(const std::string& s)
+{
+    for (char c: s)
+    {
+        if (c < '0' || c > '9')
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
 
 void print_intro()
 {
@@ -28,7 +47,7 @@ std::string get_game()
         }
         else
         {
-            std::cout << "Sorry, this game is not available here.\n";
+            std::cout << bad_input_text;
         }
     }
 
@@ -37,52 +56,107 @@ std::string get_game()
 
 int get_bet(int balance)
 {
-    int bet;
+    std::string str_bet;
+    int bet = -1;
 
     while (true)
     {
         std::cout << "Your bet: ";
-        std::cin >> bet;
+        std::cin >> str_bet;
+
+        if (check_if_number(str_bet))
+        {
+            bet = std::stoi(str_bet);
+        }
 
         if (bet >= 0 && bet <= balance)
         {
             break;
         }
-        else
-        {
-            std::cout << "Stop joking around, this is a serious place.\n";
-        }
+
+        std::cout << bad_input_text;
     }
 
     return bet;
 }
 
-int num_game(int bet)
+int get_random_number(int a, int b)
 {
-    int guess;
-
-    std::cout << "Choose your number: ";
-    std::cin >> guess;
-
     std::random_device dev;
     std::mt19937 rng(dev());
-    std::uniform_int_distribution<std::mt19937::result_type> dist(1, 7);
+    std::uniform_int_distribution<std::mt19937::result_type> dist(a, b);
 
     int number = dist(rng);
     std::cout << "The number: " << number << std::endl;
 
+    return number;
+}
+
+int num_game(int bet)
+{
+    std::string str_guess;
+    int guess = -1;
+
+    while (true)
+    {
+        std::cout << "Choose your number: ";
+        std::cin >> str_guess;
+
+        if (check_if_number(str_guess))
+        {
+            guess = std::stoi(str_guess);
+        }
+
+        if (guess >= 1 && guess <= 7)
+        {
+            break;
+        }
+
+        std::cout << bad_input_text;
+    }
+
+    int number = get_random_number(1, 7);
+
     if (guess == number)
     {
-        std::cout << "Congratulations! You won $" << bet * 5 << "!\n";
+        std::cout << won_text << bet * 5 << "!\n";
         return bet * 5;
     }
     else {
-        std::cout << "Sorry, you lost your $" << bet << ".\n";
-        return -bet;
+        std::cout << lost_text << bet << ".\n";
+        return 0;
     }
 }
 
+int sign_game(int bet)
+{
+    char guess = ' ';
+    std::vector<char> options = {'-', '0', '+'};
 
+    while (std::find(options.begin(), options.end(), guess) == options.end())
+    {
+        std::cout << "Choose your sign (-/0/+): ";
+        std::cin >> guess;
+    }
+
+    int number = get_random_number(-10, 10);
+
+    if ((guess == '-' && number < 0) || (guess == '+' && number > 0))
+    {
+        std::cout << won_text << bet * 2 << "!\n";
+        return bet * 2;
+    }
+    else if (guess == '0' && number == 0)
+    {
+        std::cout << won_text << bet * 10 << "!\n";
+        return bet * 10;
+    }
+    else
+    {
+        std::cout << lost_text << bet << ".\n";
+        return 0;
+    }
+}
 
 int main() {
     print_intro();
@@ -95,12 +169,18 @@ int main() {
     {
         game = get_game();
         bet = get_bet(balance);
+        balance -= bet;
 
         if (game == "num")
         {
             balance += num_game(bet);
-            std::cout << "Balance: $" << balance << "\n\n";
         }
+        else
+        {
+            balance += sign_game(bet);
+        }
+
+        std::cout << "Balance: $" << balance << "\n\n";
     }
 
     std::cout << "Well, there goes your welcoming gift, next time come with your own money to test the luck again!\n";
